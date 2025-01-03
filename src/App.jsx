98be8272d9ccc20c212653ba4e50bc7568c8cc3f
ltherisod <lauratherisod@gmail.com>
2025-01-02 21:08:05 -0300

    primer clase

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+const bienvenida = 'Bienvenidos a mi app'
+  return (
+    <div>
+      <h1 className='titulo'>Hola chicos! 😊 ❤️‍🔥</h1>
+      <p style={{color:'purple', fontWeight:'bold'}}>{bienvenida}</p>
+    </div>
+  )
+}
+
+export default App
